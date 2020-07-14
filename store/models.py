bd1c25338191e24af1595cdaf5d77e0143e8d2da
tal_bar_or <tbaror@gmail.com>
2020-07-14 23:04:28 +0300

    start cart view

--- store/models.py
+++ store/models.py
@@ -36,7 +36,7 @@ class Order(models.Model):
     transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
